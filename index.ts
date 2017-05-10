2aa9232be1c1574469c603f1f476f9a2319402b0
James Monger <jameskmonger@hotmail.co.uk>
2017-05-10 16:05:59 +0100

    Roll own signatures

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 /// <reference types="knockout" />
 import { ValidationFunction } from "@neworbit/validation";
 
+interface Observable<T> extends SettableObservable<T> {
+    (): T;
+    subscribe(callback: (newValue: T[]) => void): void;
+}
+
+interface SettableObservable<T> {
+    (value: T | null): void;
+}
+
 const validate = <T>(
     validators: Array<ValidationFunction<T>>, 
-    valueObservable: KnockoutObservable<T>, 
-    errorObservable: KnockoutObservableArray<string>
+    valueObservable: Observable<T>, 
+    errorObservable: SettableObservable<string>
 ) => {
-    
+
 };
 
 export {
-    validate
+    SettableObservable,
+    Observable,
+    validate    
 };
